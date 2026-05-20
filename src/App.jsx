90c69e9c6d602646ea856a06bf94c24ee91b6d08
ikiromaowiyedamiete-afk <ikiromaowiyedamiete@gmail.com>
2026-05-20 18:21:26 +0100

    add

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,12 @@ import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
+
   return (
-    <div className="App">
-      <div className="container">
-        <div className="left">
-          <h1>Threads</h1>
-          <p>Threads is a new social media platform developed by Meta, the company behind Facebook and Instagram. It is designed to be a more private and intimate space for users to connect with their close friends and family.</p>
-          <button className="download-btn">Download Now</button>
-        </div>
-        <div className="right">
-          <img src={heroImg} alt="Hero" />
-        </div>
-      </div>
+    <div className="min-h-screen flex items-center justify-center bg-blue-500">
+      <h1 className="text-white text-4xl font-bold">
+        React + Tailwind is working 🚀
+      </h1>
     </div>
   )
 }
