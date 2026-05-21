cf4c141d6f247416cca46f3549149261f745471d
ikiromaowiyedamiete-afk <ikiromaowiyedamiete@gmail.com>
2026-05-22 00:13:17 +0100

    add

--- src/pages/shopfront/Home.jsx
+++ src/pages/shopfront/Home.jsx
@@ -5,12 +5,14 @@ function Home() {
         <>
         <Navbar />
         <div className="min-h-screen flex items-center justify-center bg-gray-900 text-white">
-            <h1 className="text-4xl font-bold">
+            <div className="text-center">
+            <h1 className="text-5xl font-bold text-white">
                 Welcome to KYZ-THREADS
             </h1>
-            <p className="mt-3 text-gray-600">
+            <p className="mt-4 text-gray-600 text-lg">
                 Discover styles that fit you 
             </p>
+        </div>
         </div>
 
         <Footer />
